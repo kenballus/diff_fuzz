fa3890788e1e8718def16785c88f2eb89456c819
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-05-09 14:08:14 -0400

    change confusing var name

--- diff_fuzz.py
+++ diff_fuzz.py
@@ -257,7 +257,7 @@ def main() -> None:
         print(f"Starting generation {generation}.", file=sys.stderr)
         with multiprocessing.Pool(processes=os.cpu_count()) as pool:
             # run the programs on the things in the input queue.
-            fingerprints_and_statuses_and_stdouts = tqdm(
+            fingerprint_and_statuses_and_stdouts = tqdm(
                 pool.imap(run_executables, input_queue),
                 desc="Running targets",
                 total=len(input_queue),
@@ -266,7 +266,7 @@ def main() -> None:
             mutation_candidates: List[bytes] = []
 
             for current_input, (fingerprint, statuses, stdouts) in zip(
-                input_queue, fingerprints_and_statuses_and_stdouts
+                input_queue, fingerprint_and_statuses_and_stdouts
             ):
                 # If we found something new, mutate it and add its children to the input queue
                 # If we get one program to fail while another succeeds, then we're doing good.
