7c8a3bd59492c6910c50c44ea73b5a6b7b7500f0
JohnJamesUtley <jjutley231@gmail.com>
2023-06-05 11:18:18 -0400

    Jsonified tree from pyasn1 target

--- targets/pyasn1/pyasn1_target.py
+++ targets/pyasn1/pyasn1_target.py
@@ -1,12 +1,26 @@
-from sys import stdin, path
-print(path)
+from sys import stdin
 from os import _exit
 from pyasn1.codec.der.decoder import decode
 import afl
-afl.init()
 
-asn = stdin.buffer.read()
-x = decode(asn)
-print(f"{x}")
+def build_tree(base) -> str:
+    tag: int = base.tagSet.superTags[0][2]
+    tag_json: str = f"{{\"tag\" : \"{tag}\", \"value\" : "
 
-_exit(0)
+    if tag == 16 or tag == 17:
+        return tag_json + "[" + ",".join(f"{build_tree(z)}" for z in base.components) + "]}"
+    else:
+        # TODO: Base-64 Value
+        return tag_json + f"\"{base}\"}}"
+    
+
+def main() -> None:
+    afl.init()
+    asn = stdin.buffer.read()
+    x = decode(asn)[0]
+    print(build_tree(x))
+
+    _exit(0)
+
+if __name__ == "__main__":
+    main()
