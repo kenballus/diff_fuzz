504b483b9f6c6eb5830f2146f3fceaba01a4800d
JohnJamesUtley <jjutley231@gmail.com>
2023-06-05 16:11:07 -0400

    Normalizes output of asn parsers and base64 encodes it

--- targets/asn1crypto/asn1crypto_target.py
+++ targets/asn1crypto/asn1crypto_target.py
@@ -1,11 +1,30 @@
 from sys import stdin
 from os import _exit
 import afl
+from base64 import b64encode
+
 from asn1crypto.core import load
 
-afl.init()
-asn = stdin.buffer.read()
-x = load(asn, strict=True)
-print(f"{x}")
+def build_tree(base) -> str:
+    tag: int = base.tag
+    tag_json: str = f"{{\"tag\":\"{tag}\",\"value\":"
+    if tag == 16 or tag == 17:
+        return tag_json + "[" + ",".join(f"{build_tree(base[i])}" for i in range(len(base))) + "]}"
+    
+    if tag == 3:
+        value = ''.join(map(str, base.native))
+    else:
+        value = base.native
 
-_exit(0)
+    return tag_json + f"\"{b64encode(str(value).encode('utf-8')).decode('ascii')}\"}}"
+
+def main() -> None:
+    afl.init()
+    asn = stdin.buffer.read()
+    x = load(asn, strict=True)
+    print(build_tree(x))
+
+    _exit(0)
+
+if __name__ == "__main__":
+    main()
--- targets/pyasn1/pyasn1_target.py
+++ targets/pyasn1/pyasn1_target.py
@@ -1,18 +1,29 @@
 from sys import stdin
 from os import _exit
 from pyasn1.codec.der.decoder import decode
+from datetime import datetime
+from base64 import b64encode
 import afl
 
 def build_tree(base) -> str:
     tag: int = base.tagSet.superTags[0][2]
-    tag_json: str = f"{{\"tag\" : \"{tag}\", \"value\" : "
-
+    tag_json: str = f"{{\"tag\":\"{tag}\",\"value\":"
     if tag == 16 or tag == 17:
         return tag_json + "[" + ",".join(f"{build_tree(z)}" for z in base.components) + "]}"
-    else:
-        # TODO: Base-64 Value
-        return tag_json + f"\"{base}\"}}"
     
+    if tag == 4:
+        value = bytes(base)
+    elif tag == 5:
+        value = None
+    elif tag == 23 or tag == 24:
+        try:
+            value = datetime.strptime(str(base), r'%y%m%d%H%M%S%z')
+        except ValueError as e:
+            value = datetime.strptime(str(base), r'%Y%m%d%H%M%S%z')
+    else:
+        value = base
+
+    return tag_json + f"\"{b64encode(str(value).encode('utf-8')).decode('ascii')}\"}}"
 
 def main() -> None:
     afl.init()
